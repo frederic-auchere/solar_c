Fixed computation of first guess sphere

optics/surfaces.py
@@ -15,7 +15,9 @@ class Point:
 
 def sphere_from_four_points(p1, p2, p3, p4):
     U = lambda a, b, c, d, e, f, g, h: (a.z - b.z)*(c.x*d.y - d.x*c.y) - (e.z - f.z)*(g.x*h.y - h.x*g.y)
-    D = lambda x, y, a, b, c: a.x*(b.y - c.y) + b.x*(c.y-a.y) + c.x*(a.y - b.y)
+    D = lambda x, y, a, b, c: a.__getattribute__(x)*(b.__getattribute__(y) - c.__getattribute__(y)) +\
+                              b.__getattribute__(x)*(c.__getattribute__(y) - a.__getattribute__(y)) +\
+                              c.__getattribute__(x)*(a.__getattribute__(y) - b.__getattribute__(y))
     E = lambda x, y: (r1*D(x, y, p2, p3, p4) - r2*D(x, y, p3, p4, p1) +
                       r3*D(x, y, p4, p1, p2) - r4*D(x, y, p1, p2, p3)) / uvw
     u = U(p1, p2, p3, p4, p2, p3, p4, p1)
@@ -25,9 +27,9 @@ def sphere_from_four_points(p1, p2, p3, p4):
     if uvw == 0.0:
         raise ValueError('The points are coplanar')
     r1, r2, r3, r4 = p1.radius ** 2, p2.radius ** 2, p3.radius ** 2, p4.radius ** 2
-    x0, y0, z0 = E(1, 2), E(2, 0), E(0, 1)
+    x0, y0, z0 = E('y', 'z'), E('z', 'x'), E('x', 'y')
     p = Point(p1.x - x0, p1.y - y0, p1.z - z0)
-    return p.radius
+    return x0, y0, z0, p.radius
 
 
 class Surface:
@@ -47,10 +49,10 @@ class Toroidal(Surface):
         self.rr = rr
 
     def sag(self, x, y):
-        c = 1/self.rc
-        y2 = (y - self.dy)**2
-        zy = y2 * c / (1 + np.sqrt(1 - y2 * c**2))
-        zx = self.rr - zy - np.sqrt((self.rr - zy)**2 - (x - self.dx)**2)
+        c = 1 / self.rc
+        y2 = (y - self.dy) ** 2
+        zy = y2 * c / (1 + np.sqrt(1 - y2 * c ** 2))
+        zx = self.rr - zy - np.sqrt((self.rr - zy) ** 2 - (x - self.dx) ** 2)
         return zx + zy
 
 
@@ -62,7 +64,7 @@ class EllipticalGrating(Surface):
         self.c = c
 
     def sag(self, x, y):
-        u2 = (self.a * (x - self.dx))**2 + (self.b * (y - self.dy))**2
+        u2 = (self.a * (x - self.dx)) ** 2 + (self.b * (y - self.dy)) ** 2
         return u2 * self.c / (1 + np.sqrt(1 - u2))
 
 
@@ -74,8 +76,8 @@ class Standard(Surface):
 
     def sag(self, x, y):
         c = 1 / self.r
-        r2 = (x - self.dx)**2 + (y - self.dy)**2
-        return self.dz + r2 * c / (1 + np.sqrt(1 - (1 + self.k) * r2 * c**2))
+        r2 = (x - self.dx) ** 2 + (y - self.dy) ** 2
+        return self.dz + r2 * c / (1 + np.sqrt(1 - (1 + self.k) * r2 * c ** 2))
 
 
 class Sphere(Standard):
@@ -141,10 +143,10 @@ class Substrate:
         points = []
         for px, py in zip((x.min(), x.max(), x_mean, x_mean), (y_mean, y_mean, y.min(), y.max())):
             points.append(Point(px, py, self.surface.sag(px, py)))
-        initial_radius = sphere_from_four_points(*points)
-
+        x0, y0, z0, r0 = sphere_from_four_points(*points)
+        print(x0, y0, z0, r0)
         mini = minimize(self._sphere_min,
-                        np.array([initial_radius, self.surface.dx, self.surface.dy, 0]),
+                        np.array([r0, x0, y0, z0 - r0]),
                         args=(x, y), method='Powell')
         return Sphere(*mini.x)
 
@@ -154,9 +156,9 @@ class Substrate:
         y_min = self.aperture.dy - self.aperture.y_width / 2
         y_max = self.aperture.dy + self.aperture.y_width / 2
         if nx is None:
-            nx = int((x_max - x_min)/0.1)
+            nx = int((x_max - x_min) / 0.1)
         if ny is None:
-            ny = int((y_max - y_min)/0.1)
+            ny = int((y_max - y_min) / 0.1)
         return np.meshgrid(np.linspace(x_min, x_max, nx), np.linspace(y_min, y_max, ny))
 
     def _sphere_min(self, coefficients, x, y):
@@ -167,7 +169,7 @@ class Substrate:
         return np.mean(delta ** 2)
 
     def interferogram(self):
-        x, y, = self.meshgrid()
+        x, y = self.meshgrid()
         delta = self.sag(x, y) - self.best_sphere.sag(x, y)
 
         wvl = 632e-6