867c5e214808a1fa7407a77ad11e755004201e75
fauchere <frederic.auchere@gmail.com>
2024-04-28 10:03:03 +0200

    Added substrate names

--- optical/__init__.py
+++ optical/__init__.py
@@ -8,21 +8,35 @@ lw_surface = EllipticalGrating(1 / 1008.9554166, 1 / 1010.2811818, 1933.255026,
                                dx=0, dy=2.0386372, dz=0,
                                alpha=0.346498, beta=0.7733422, gamma=0, degrees=True)
 lw_aperture = RectangularAperture(17.4, 34.8, dx=-17.4 / 2 - 0.15 / 2)
-lw_substrate = Substrate(lw_surface, lw_aperture, useful_area)
+lw_substrate = Substrate(lw_surface,
+                         lw_aperture,
+                         useful_area,
+                         name='LW')
 # Dummy substrate with spherical surface
-dummy_lw_aperture = RectangularAperture(61.925, 86.188, dx=-61.925 / 2 - 0.15 / 2 + 20)
-dummy_lw_substrate = Substrate(lw_substrate.best_sphere, copy.deepcopy(dummy_lw_aperture))
+spherical_lw_aperture = RectangularAperture(61.925, 86.188, dx=-61.925 / 2 - 0.15 / 2 + 20)
+spherical_lw_substrate = Substrate(lw_substrate.best_sphere,
+                               copy.deepcopy(spherical_lw_aperture),
+                               name='Spherical LW')
 # Rectangular substrate pre-cutting to octagon
-rectangular_lw_substrate = Substrate(copy.deepcopy(lw_substrate.surface), copy.deepcopy(dummy_lw_aperture))
+rectangular_lw_substrate = Substrate(copy.deepcopy(lw_substrate.surface),
+                                     copy.deepcopy(spherical_lw_aperture),
+                                     name='Rectangular LW')
 
 sw_surface = Standard(516.0274, -0.522870,
                       dx=31.8380922, dy=15.0690780, dz=2.446336,
                       alpha=3.1515375, beta=-5.0875302, gamma=0, degrees=True)
 sw_aperture = RectangularAperture(17.4, 34.8, dx=17.4 / 2 + 0.15 / 2)
-sw_substrate = Substrate(sw_surface, sw_aperture, useful_area)
+sw_substrate = Substrate(sw_surface,
+                         sw_aperture,
+                         useful_area,
+                         name='SW')
 # Dummy substrate with spherical surface
-dummy_sw_aperture = RectangularAperture(61.925, 86.188, dx=61.925 / 2 + 0.15 / 2 - 20)
-dummy_sw_substrate = Substrate(sw_substrate.best_sphere, copy.deepcopy(dummy_sw_aperture))
+spherical_sw_aperture = RectangularAperture(61.925, 86.188, dx=61.925 / 2 + 0.15 / 2 - 20)
+spherical_sw_substrate = Substrate(sw_substrate.best_sphere,
+                                   copy.deepcopy(spherical_sw_aperture),
+                                   name='Spherical SW')
 # Rectangular substrate pre-cutting to octagon
-rectangular_sw_substrate = Substrate(copy.deepcopy(sw_substrate.surface), copy.deepcopy(dummy_sw_aperture))
+rectangular_sw_substrate = Substrate(copy.deepcopy(sw_substrate.surface),
+                                     copy.deepcopy(spherical_sw_aperture),
+                                     name='Rectangular SW')
 
