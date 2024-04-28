992ddb8d86bb6dcf81777a939d64a65e05b5a8fa
fauchere <frederic.auchere@gmail.com>
2024-04-28 09:47:19 +0200

    Jupyter notebook for sag validation

--- optical/__init__.py
+++ optical/__init__.py
@@ -1,4 +1,5 @@
-from optics import Standard, EllipticalGrating, Toroidal, CircularAperture, RectangularAperture, Substrate
+import copy
+from optics import Standard, EllipticalGrating, CircularAperture, RectangularAperture, Substrate
 
 # Useful area defined in the (x, y) plane of the EGA coordinate system, i.e. in the middle of the two halves
 useful_area = CircularAperture(34.2)
@@ -8,13 +9,20 @@ lw_surface = EllipticalGrating(1 / 1008.9554166, 1 / 1010.2811818, 1933.255026,
                                alpha=0.346498, beta=0.7733422, gamma=0, degrees=True)
 lw_aperture = RectangularAperture(17.4, 34.8, dx=-17.4 / 2 - 0.15 / 2)
 lw_substrate = Substrate(lw_surface, lw_aperture, useful_area)
+# Dummy substrate with spherical surface
 dummy_lw_aperture = RectangularAperture(61.925, 86.188, dx=-61.925 / 2 - 0.15 / 2 + 20)
-dummy_lw_substrate = Substrate(lw_substrate.best_sphere, dummy_lw_aperture)
+dummy_lw_substrate = Substrate(lw_substrate.best_sphere, copy.deepcopy(dummy_lw_aperture))
+# Rectangular substrate pre-cutting to octagon
+rectangular_lw_substrate = Substrate(copy.deepcopy(lw_substrate.surface), copy.deepcopy(dummy_lw_aperture))
 
 sw_surface = Standard(516.0274, -0.522870,
                       dx=31.8380922, dy=15.0690780, dz=2.446336,
                       alpha=3.1515375, beta=-5.0875302, gamma=0, degrees=True)
 sw_aperture = RectangularAperture(17.4, 34.8, dx=17.4 / 2 + 0.15 / 2)
 sw_substrate = Substrate(sw_surface, sw_aperture, useful_area)
+# Dummy substrate with spherical surface
 dummy_sw_aperture = RectangularAperture(61.925, 86.188, dx=61.925 / 2 + 0.15 / 2 - 20)
-dummy_sw_substrate = Substrate(sw_substrate.best_sphere, dummy_sw_aperture)
+dummy_sw_substrate = Substrate(sw_substrate.best_sphere, copy.deepcopy(dummy_sw_aperture))
+# Rectangular substrate pre-cutting to octagon
+rectangular_sw_substrate = Substrate(copy.deepcopy(sw_substrate.surface), copy.deepcopy(dummy_sw_aperture))
+
--- plot_grating_sag_vs_best_sphere.py
+++ plot_grating_sag_vs_best_sphere.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from optical import sw_substrate, lw_substrate
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -9,16 +10,16 @@ fig, axes = plt.subplots(2, 3, figsize=(13, 12))
 
 for name, substrate, ax_row in zip(('SW', 'LW'), (sw_substrate, lw_substrate), axes):
 
-    ellipse_z = substrate.sag()
-    fit = substrate.best_sphere.sag(substrate.grid())
-    delta = (ellipse_z - fit) * 1e6
+    substrate_z = substrate.sag()
+    delta = substrate.sag_from_sphere() * 1e6
     dl = substrate.interferogram()
 
     titles = [f"{name} z sag", f"from best sphere (R={substrate.best_sphere.r:3.2f} mm)", "Simulated interferogram"]
     clabels = ["sag [mm]", "sag difference [nm]", "intensity"]
 
-    for ax, data, title, clabel in zip(ax_row, (ellipse_z, delta, dl), titles, clabels):
-        im = ax.imshow(data, extent=substrate.aperture.limits, origin='lower')
+    for ax, data, title, clabel in zip(ax_row, (substrate_z, delta, dl), titles, clabels):
+        v_min, v_max = np.nanpercentile(data[~data.mask], [1, 99])
+        im = ax.imshow(data, extent=substrate.aperture.limits, origin='lower', vmin=v_min, vmax=v_max)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='5%', pad=0.1)
         fig.colorbar(im, cax=cax, label=clabel)
