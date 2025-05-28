90d95eeba857d39ef977cab05df1eb55d9445c2b
fauchere <frederic.auchere@gmail.com>
2025-05-28 14:34:20 +0200

    Added command line interface

---
+++ optical/cli.py
@@ -0,0 +1,25 @@
+import argparse
+from optical.zygo import EGAFit
+
+
+def main(xlsx_file, outfile=None):
+    fitter = EGAFit.from_xlsx(xlsx_file)
+    fit = fitter.fit()
+    fitter.make_report(outfile=outfile)
+
+
+def cli():
+    """
+    Command line interface
+    :return:
+    """
+    parser = argparse.ArgumentParser(prog="Surface Fitting",
+                                     description="Processes a sequence of files based as described in a xls file")
+    parser.add_argument("xlsx_file", help="Input .excel filename")
+    parser.add_argument("-o", "--outfile",
+                        help="Output filename.",
+                        type=str)
+
+    args = parser.parse_args()
+
+    main(**vars(args))
