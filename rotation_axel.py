e3a90400f941ee65f627676ddbd139fc4c11f74d
fauchere <frederic.auchere@gmail.com>
2026-03-17 17:27:52 +0100

    Working on rotation_axel

--- rotation_axel.py
+++ rotation_axel.py
@@ -32,22 +32,22 @@ zygo_data = SagData(files_zygo, dx=dx_zygo, dy=dy_zygo, theta=0, binning=1, auto
 alpha, beta, gamma = rectangular_sw_substrate.tip_tilt_from_normal()
 measured_surface = surfaces.MeasuredSurface(zygo_data, alpha=alpha, beta=beta, gamma=gamma) #est ce que on laisse ces angles ? self.tip_tilt_to_normal ?
 
-measured_substrate = surfaces.Substrate(measured_surface, rectangular_sw_substrate.aperture, rectangular_sw_substrate.useful_area)
+measured_substrate = surfaces.Substrate(measured_surface, rectangular_sw_substrate.aperture, rectangular_sw_substrate.useful_area, x_grid_step=0.5)
 
 
 #grille nominal surface
-valid_only = True
-ox, oy = rectangular_sw_substrate.grid()
+#valid_only = True
+ox, oy = measured_substrate.grid()
 
 xy = None
-valid = np.logical_and(~rectangular_sw_substrate.sag(xy).mask, ~np.isnan(rectangular_sw_substrate.sag(xy).data)) if valid_only else Ellipsis
+# valid = np.logical_and(~rectangular_sw_substrate.sag(xy).mask, ~np.isnan(rectangular_sw_substrate.sag(xy).data)) if valid_only else Ellipsis
 x, y = ox, oy
 
 xyz = np.stack((x.ravel(), y.ravel(), np.zeros(x.size), np.ones(x.size)))
 
 x, y, _= measured_substrate.surface.rotation_matrix() @ xyz #shape 3xN
-z = measured_substrate.sag((x,y)) #x et y tournés depuis EGA-O vers normal et sag calculé en ces points là
-plt.imshow(z.reshape(ox.shape), origin = 'lower', extent = measured_substrate.aperture.limits)
+z = measured_substrate.sag((x,y)).data / 1e6 #x et y tournés depuis EGA-O vers normal et sag calculé en ces points là
+#plt.imshow(z.reshape(ox.shape), origin = 'lower', extent = measured_substrate.aperture.limits)
 # valid_mask = np.isfinite(sag_in_normal)
 # x_valid = x[valid_mask]
 # y_valid = y[valid_mask]
@@ -60,7 +60,7 @@ xyz = np.stack((x, y, z, np.ones(x.size))) #shape 4x10201
 # print(np.count_nonzero(np.isnan(sag_valid)))
 # print(np.min(sag_in_normal), np.max(sag_valid))
 
-alpha_corr = np.deg2rad(0.0001)
+alpha_corr = np.deg2rad(0)
 beta_corr = np.deg2rad(10)
 correction_tilt = Rotation.from_euler('xyz', (alpha_corr, beta_corr, 0)).as_matrix() #shape 3x3
 translation = np.array([[1, 0, 0, 0],
@@ -73,7 +73,7 @@ nx, ny, nz = correction_tilt @ xyz  #[:3, :] # shape 3x3 @ shape 3x10201 -> 3x10
 
 xyz = np.stack((nx, ny, nz, np.ones(x.size))) #shape 4x10201
 
-nx, ny, nz = rectangular_sw_substrate.surface.inverse_rotation_matrix() @ xyz
+nx, ny, nz = measured_substrate.surface.inverse_rotation_matrix() @ xyz
 
 # print(np.count_nonzero(np.isnan(nx)))
 # print(np.count_nonzero(np.isnan(ny)))
@@ -94,7 +94,9 @@ nx, ny, nz = rectangular_sw_substrate.surface.inverse_rotation_matrix() @ xyz
 # plt.show()
 #sag_corrige_tilt_interpolated_substrate_grid = griddata(points, values, (ox,oy), method='cubic', rescale = False)
 
-nz = griddata((nx, ny), nz, (ox, oy), method='cubic', rescale=False).reshape(ox.shape)
+valid = np.logical_and(np.isfinite(nx), np.isfinite(ny))
+sag = griddata((nx[valid], ny[valid]), nz[valid], (ox, oy), method='nearest', rescale=False).reshape(ox.shape)
+plt.imshow(sag, origin='lower')
 
 outfile=r'Y:/02- Engineering/08 - Metrology/01 - Optics/07 - Measurements/FM/SW/FM_SW_SN1/Zygo/Form/20260223/substrates_template_FM_form_casquette_Bertin_bascule_nouveau_code_test_nina.fits'
 header = fits.header.Header()
