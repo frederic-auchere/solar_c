5379ff98cbb7a279c4b12bd65ec30d1ee4b8c2eb
fauchere <frederic.auchere@gmail.com>
2026-03-18 10:53:39 +0100

    Working rotation_fred!

--- rotation_axel.py
+++ rotation_axel.py
@@ -1,28 +1,12 @@
-import os.path
-import numpy as np
-import h5py
-import inspect
-from factory import Parameterized, Parameter
-from optics import surfaces
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-from matplotlib.patches import Ellipse
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from astropy.stats import sigma_clip
-from astropy.io import fits
-import warnings
 from scipy.spatial.transform import Rotation
 from scipy.interpolate import griddata
-from scipy.interpolate import CloughTocher2DInterpolator
-
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from optical import rectangular_sw_substrate
 from optics.zygo import SagData
-import os
 from optics import surfaces
 import numpy as np
+from optics.geometry import NormalVector
 
 files_zygo = r'Y:\02- Engineering/08 - Metrology/01 - Optics/07 - Measurements/FM/SW/FM_SW_SN1/Zygo/Form/20260223/substrates_template_FM_form_casquette_Bertin.fits'
 dx_zygo = 200
@@ -30,73 +14,67 @@ dy_zygo = 432
 
 zygo_data = SagData(files_zygo, dx=dx_zygo, dy=dy_zygo, theta=0, binning=1, auto_crop=True)
 alpha, beta, gamma = rectangular_sw_substrate.tip_tilt_from_normal()
-measured_surface = surfaces.MeasuredSurface(zygo_data, alpha=alpha, beta=beta, gamma=gamma) #est ce que on laisse ces angles ? self.tip_tilt_to_normal ?
+measured_surface = surfaces.MeasuredSurface(zygo_data, alpha=alpha, beta=beta, gamma=gamma)
 
-measured_substrate = surfaces.Substrate(measured_surface, rectangular_sw_substrate.aperture, rectangular_sw_substrate.useful_area, x_grid_step=0.5)
+measured_substrate = surfaces.Substrate(measured_surface, rectangular_sw_substrate.aperture, rectangular_sw_substrate.useful_area)
 
+grid_step = 0.5  # [mm]
+nx = int((rectangular_sw_substrate.limits[1] - rectangular_sw_substrate.limits[0]) / grid_step)
+ny = int((rectangular_sw_substrate.limits[3] - rectangular_sw_substrate.limits[2]) / grid_step)
+x_min = rectangular_sw_substrate.limits[0]
+x_max = x_min + grid_step * nx
+y_min = rectangular_sw_substrate.limits[2]
+y_max = y_min + grid_step * ny
+ox, oy = np.meshgrid(np.linspace(x_min, x_max, nx + 1), np.linspace(y_min, y_max, ny + 1))
 
-#grille nominal surface
-#valid_only = True
-ox, oy = measured_substrate.grid()
+nominal_sag = rectangular_sw_substrate.sag((ox, oy)).data
 
-xy = None
-# valid = np.logical_and(~rectangular_sw_substrate.sag(xy).mask, ~np.isnan(rectangular_sw_substrate.sag(xy).data)) if valid_only else Ellipsis
-x, y = ox, oy
+xyz = np.stack((ox.ravel(), oy.ravel(), nominal_sag.ravel(), np.ones(ox.size)))
 
-xyz = np.stack((x.ravel(), y.ravel(), np.zeros(x.size), np.ones(x.size)))
-
-x, y, _= measured_substrate.surface.rotation_matrix() @ xyz #shape 3xN
-z = measured_substrate.sag((x,y)).data / 1e6 #x et y tournés depuis EGA-O vers normal et sag calculé en ces points là
-#plt.imshow(z.reshape(ox.shape), origin = 'lower', extent = measured_substrate.aperture.limits)
-# valid_mask = np.isfinite(sag_in_normal)
-# x_valid = x[valid_mask]
-# y_valid = y[valid_mask]
-# sag_valid = sag_in_normal[valid_mask]
-# print("Shape:", sag_valid.shape)
-# print("Nombre de NaN:", np.count_nonzero(np.isnan(sag_valid)))
+x, y, z = measured_substrate.surface.rotation_matrix() @ xyz
+z += measured_substrate.sag((x,y)).data / 1e6
 
 xyz = np.stack((x, y, z, np.ones(x.size))) #shape 4x10201
 
-# print(np.count_nonzero(np.isnan(sag_valid)))
-# print(np.min(sag_in_normal), np.max(sag_valid))
 
-alpha_corr = np.deg2rad(0)
-beta_corr = np.deg2rad(10)
-correction_tilt = Rotation.from_euler('xyz', (alpha_corr, beta_corr, 0)).as_matrix() #shape 3x3
+dx = -45.74
+dy = -28.389
+dx = -45.892
+dy = -28.339 # violet
+# dx = -45.81 # rouge
+# dy = -28.292
+dx = 0.173
+dy = -0.03
+p = np.sqrt(dx ** 2 + dy **2)
+dz = p * np.tan(np.acos(p / rectangular_sw_substrate.best_sphere.r))
+normal_vector = NormalVector(dx, dy, dz)
+delta_alpha = -np.arctan2(normal_vector.y, normal_vector.z)
+delta_beta = np.arctan2(normal_vector.x, np.sqrt(normal_vector.y ** 2 + normal_vector.z ** 2))
+print(60 * np.degrees(delta_alpha), 60 * np.degrees(delta_beta))
+
+correction_tilt = Rotation.from_euler('xyz', (-delta_alpha, -delta_beta, 0)).as_matrix() #shape 3x3
 translation = np.array([[1, 0, 0, 0],
                         [0, 1, 0, 0],
                         [0, 0, 1, 0]])
 correction_tilt = correction_tilt @ translation
 
-# xyz = np.asarray(xyz) #sinon probleme avec le masque
 nx, ny, nz = correction_tilt @ xyz  #[:3, :] # shape 3x3 @ shape 3x10201 -> 3x10201
 
 xyz = np.stack((nx, ny, nz, np.ones(x.size))) #shape 4x10201
 
 nx, ny, nz = measured_substrate.surface.inverse_rotation_matrix() @ xyz
 
-# print(np.count_nonzero(np.isnan(nx)))
-# print(np.count_nonzero(np.isnan(ny)))
-# print(np.count_nonzero(np.isnan(nz)))
-
-
-# points = np.column_stack((nx.ravel(), ny.ravel()))
-# values = nz.ravel()
-#
-#
-# print("ox range:", ox.min(), ox.max())
-# print("oy range:", oy.min(), oy.max())
-#
-# print("nx range:", np.nanmin(nx), np.nanmax(nx))
-# print("ny range:", np.nanmin(ny), np.nanmax(ny))
-# plt.scatter(nx.ravel(), ny.ravel(), c=nz.ravel(), s=5)
-# plt.scatter(ox.ravel(), oy.ravel(), marker='x', color='red')
-# plt.show()
-#sag_corrige_tilt_interpolated_substrate_grid = griddata(points, values, (ox,oy), method='cubic', rescale = False)
-
 valid = np.logical_and(np.isfinite(nx), np.isfinite(ny))
-sag = griddata((nx[valid], ny[valid]), nz[valid], (ox, oy), method='nearest', rescale=False).reshape(ox.shape)
-plt.imshow(sag, origin='lower')
+sag = griddata((nx[valid], ny[valid]), nz[valid], (ox, oy), method='cubic', rescale=False).reshape(ox.shape)
+
+fig, axes = plt.subplots(1, 3)
+im1 = axes[0].imshow(sag, origin='lower', extent=rectangular_sw_substrate.limits)
+plt.colorbar(im1, ax=axes[0], label='z sag [mm]')
+sag_difference = (sag - nominal_sag) * 1e6
+im2 = axes[1].imshow(sag_difference, origin='lower', extent=rectangular_sw_substrate.limits, vmin=-20000, vmax=20000)
+plt.colorbar(im2, ax=axes[1], label = r'z sag difference [nm]')
+im3 = axes[2].imshow(measured_substrate.sag((ox, oy)) - sag_difference, origin='lower', extent=rectangular_sw_substrate.limits, vmin=-50, vmax=50)
+plt.colorbar(im3, ax=axes[2], label = r'z sag difference difference [nm]')
 
 outfile=r'Y:/02- Engineering/08 - Metrology/01 - Optics/07 - Measurements/FM/SW/FM_SW_SN1/Zygo/Form/20260223/substrates_template_FM_form_casquette_Bertin_bascule_nouveau_code_test_nina.fits'
 header = fits.header.Header()
